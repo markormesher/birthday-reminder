b1cb8119d835887e4a8e1d199bf20d5dc5401753
Mark Ormesher <me@markormesher.co.uk>
2017-09-13 10:19:21 +0100

    Make alarm time time-zone aware

--- app/src/main/kotlin/uk/co/markormesher/birthdayreminder/AlarmSetterReceiver.kt
+++ app/src/main/kotlin/uk/co/markormesher/birthdayreminder/AlarmSetterReceiver.kt
@@ -5,22 +5,19 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import java.util.*
+import org.joda.time.DateTime
 
 class AlarmSetterReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         val reminderIntent = PendingIntent.getService(context, 0, Intent(context, ReminderService::class.java), 0)
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val calendar = with(Calendar.getInstance()) {
-            set(Calendar.DAY_OF_YEAR, 0)
-            set(Calendar.HOUR_OF_DAY, 8)
-            set(Calendar.MINUTE, 0)
-            set(Calendar.SECOND, 0)
-            return@with this
+        var firstAlarm = DateTime.now().withHourOfDay(8).withMinuteOfHour(0)
+        if (firstAlarm.isBeforeNow) {
+            firstAlarm = firstAlarm.plusDays(1)
         }
         alarmManager.cancel(reminderIntent)
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, AlarmManager.INTERVAL_DAY, reminderIntent)
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, firstAlarm.millis, AlarmManager.INTERVAL_DAY, reminderIntent)
     }
 
 }
